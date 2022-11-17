86579deb4ee02f9aee511919e8d14480b437fe18
takaki-web <104483390+takaki-web@users.noreply.github.com>
2022-11-17 23:56:44 +0900

    ページ内スクロール追加

--- js/script.js
+++ js/script.js
@@ -19,4 +19,12 @@ $('.js-l-header__burger-menu').on('click', function (event) {
   $('.js-l-header__burger-btn').trigger('click')
 })
 
-
+// ページ内スクロール
+$('a[href^="#"]').click(function () {
+  const speed = 600;
+  let href = $(this).attr("href");
+  let target = $(href == "#" || href == "" ? "html" : href);
+  let position = target.offset().top;
+  $("body,html").animate({ scrollTop: position }, speed, "swing");
+  return false;
+});
