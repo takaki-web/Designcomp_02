071048fc559f25a7264c49362502fdc8abfd5d74
takaki-web <104483390+takaki-web@users.noreply.github.com>
2022-11-25 17:36:54 +0900

    ハンバーガー背景固定追加

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-// ハンバーガーメニュー
-$(function () {
-  $('.js-l-header__burger-btn').click(function () {
-    $(this).toggleClass('active')
-
-    if ($(this).hasClass('active')) {
-      $('.js-l-header__burger-menu').addClass('active')
-    } else {
-      $('.js-l-header__burger-menu').removeClass('active')
-    }
-  })
-})
-// ページ内リンクに飛ぶ時にリンククリックしたらハンバーガーメニューが閉じるように
-$('#menu a[href]').on('click', function (event) {
-  $('.js-l-header__burger-btn').trigger('click')
-})
-// ×ボタン押したらハンバーガーメニューが閉じるように
-$('.js-l-header__burger-menu').on('click', function (event) {
-  $('.js-l-header__burger-btn').trigger('click')
-})
-
 // ページ内スクロール
 $('a[href^="#"]').click(function () {
   const speed = 600;
@@ -27,4 +6,23 @@ $('a[href^="#"]').click(function () {
   let position = target.offset().top;
   $("body,html").animate({ scrollTop: position }, speed, "swing");
   return false;
+});
+
+// ハンバーガーメニュー
+$(function () {
+  $(".js-l-header__burger-btn").click(function () {
+    $(this).toggleClass("active");
+
+    if ($(this).hasClass("active")) {
+      $(".js-l-header__burger-menu").addClass("active");
+      $("body").addClass("fixed");
+    } else {
+      $(".js-l-header__burger-menu").removeClass("active");
+      $("body").removeClass("fixed");
+    }
+  });
+});
+// ページ内リンクに飛ぶ時にリンククリックしたらハンバーガーメニューが閉じるように
+$(".l-header__burger-menu-link,.l-header__burger-menu-link a").on("click", function (event) {
+  $(".js-l-header__burger-btn").trigger("click");
 });
